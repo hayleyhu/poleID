18564454c17d3a4923936e5d8c0af6f11e25a398
hayleyhu <yuehu2018@u.northwestern.edu>
2017-03-05 18:11:44 -0600

    range and frequecy filtering

--- process_file.py
+++ process_file.py
@@ -23,28 +23,16 @@ def LLHtoECEF(lat, lon, alt):
 
 def fuse2xyz(filename):
 	fuse_file = open(filename, 'rb')
-	point_info = list()
-	lat_set = set()
-	long_set = set()
+	obj_file = open('point_cloud.obj', 'w')
 	
 	for line in fuse_file:
 		r = line.strip().split(' ')
-		point = []
+		if len(r)<4:
+			print line
 		x, y, z = LLHtoECEF(float(r[0]), float(r[1]), float(r[2]))
-		point.append(x)
-		point.append(y)
-		point.append(z)
-		lat_set.add(x)
-		long_set.add(y)
-		point_info.append(point)
-
-	obj_file = open('point_cloud.obj', 'w')
-	
-	for point in point_info:
-		line = "v " + str(point[0]) + " " + str(point[1]) + " "+ str(point[2])
-		obj_file.write(line)
-		obj_file.write("\n")
-
+		newline = "v " + str(x) + " " + str(y) + " "+ str(z)+ " "+r[3]+"\n"
+		obj_file.write(newline)
+		
 	obj_file.close()
 
 
@@ -152,25 +140,185 @@ def filterDistance(xmedian, ymedian):
 
 	outfile.close()
 
+def freqHistogram():
+	
+	obj_file = open('filtered_intensity&distance_point_cloud.obj', 'rb')
+	freqd = dict()
+	for line in obj_file:
+		r = line.strip().split(' ')
+		x = float(r[1])
+		y = float(r[2])
+		z = float(r[3]) 
+
+		adjusted_x = int(x//0.1)
+		adjusted_y = int(y//0.1)
+		freqd[(adjusted_x,adjusted_y)] = freqd.get((adjusted_x,adjusted_y),0)+1
+		
+	obj_file.close()
+	# print freqd
+	
+	frequencies = np.array(freqd.values())
+	n, bins, pathes = plt.hist(frequencies, bins=20, normed=1)
+	plt.xlabel("Frequencies")
+	plt.ylabel('Probability')
+	plt.grid(True)
+	plt.show()
+
+def filterFreq():
+	obj_file = open('filtered_intensity&distance_point_cloud.obj', 'rb')
+	point_info = list()
+	freqd = dict()
+
+	for line in obj_file:
+		r = line.strip().split(' ')
+		x = float(r[1])
+		y = float(r[2])
+		z = float(r[3]) 
+		point_info.append([x,y,z])
+		adjusted_x = int(x//0.1)
+		adjusted_y = int(y//0.1)
+		freqd[(adjusted_x,adjusted_y)] = freqd.get((adjusted_x,adjusted_y),0)+1
+	obj_file.close()
+
+	outfile = open('filtered_freq.obj', 'w')
+	for point in point_info:
+		x = point[0]
+		y = point[1]
+		adjusted_x = int(x//0.1)
+		adjusted_y = int(y//0.1)
+		if freqd[(adjusted_x, adjusted_y)] < 5:
+			continue
+		line = "v " + str(point[0]) + " " + str(point[1]) + " "+ str(point[2])
+		outfile.write(line)
+		outfile.write("\n")
+
+	outfile.close()
+
+def zHistogram():
+	
+	obj_file = open('filtered_intensity&distance_point_cloud.obj', 'rb')
+	zs = list()
+	for line in obj_file:
+		r = line.strip().split(' ')
+		z = float(r[3]) 
+		zs.append(z)
+
+	obj_file.close()
+
+	zs = np.array(zs)
+	n, bins, pathes = plt.hist(zs, bins=20, normed=1)
+	plt.xlabel("Heights(z)")
+	plt.ylabel('Probability')
+	plt.grid(True)
+	plt.show()
+
+
+def filterZ():
+	# NOT WORKING!!!! 
+	obj_file = open('filtered_intensity&distance_point_cloud.obj', 'rb')
+	point_info = list()
+	for line in obj_file:
+		r = line.strip().split(' ')
+		x = float(r[1])
+		y = float(r[2])
+		z = float(r[3]) 
+		if z<4558000:
+			continue
+		point_info.append([x,y,z])
+	obj_file.close()
+
+	outfile = open('filtered_z.obj', 'w')
+	for point in point_info:
+		line = "v " + str(point[0]) + " " + str(point[1]) + " "+ str(point[2])
+		outfile.write(line)
+		outfile.write("\n")
+	outfile.close()
+
+def rangeHistogram():
+	
+	obj_file = open('filtered_intensity&distance_point_cloud.obj', 'rb')
+	coords_dict = dict()
+	freqd = dict()
+	for line in obj_file:
+		r = line.strip().split(' ')
+		x = float(r[1])
+		y = float(r[2])
+		z = float(r[3]) 
+		# point_info.append([x,y,z])
+		adjusted_x = int(x//0.3)
+		adjusted_y = int(y//0.3)
+		if (adjusted_x, adjusted_y) not in coords_dict.keys():
+			coords_dict[(adjusted_x,adjusted_y)] = list()
+		coords_dict[(adjusted_x,adjusted_y)].append(z)
+		freqd[(adjusted_x,adjusted_y)] = freqd.get((adjusted_x,adjusted_y),0)+1	
+		
+	obj_file.close()
+
+	range_list = list()
+	for z_list in coords_dict.values():
+		rg = np.ptp(np.array(z_list))
+		range_list.append(rg)
+	range_list = np.array(range_list)
+	n, bins, pathes = plt.hist(range_list, bins=20, normed=1)
+	plt.xlabel("Range")
+	plt.ylabel('Probability')
+	plt.grid(True)
+	plt.show()
+
+	# frequencies = np.array(freqd.values())
+	# n, bins, pathes = plt.hist(frequencies, bins=20, normed=1)
+	# plt.xlabel("Frequencies")
+	# plt.ylabel('Probability')
+	# plt.grid(True)
+	# plt.show()
+
+def filterRange():
+	obj_file = open('filtered_intensity&distance_point_cloud.obj', 'rb')
+	coords_dict = dict()
+	freqd = dict()
+	point_info = list()
+	for line in obj_file:
+		r = line.strip().split(' ')
+		x = float(r[1])
+		y = float(r[2])
+		z = float(r[3]) 
+		point_info.append([x,y,z])
+		adjusted_x = int(x//0.3)
+		adjusted_y = int(y//0.3)
+		if (adjusted_x, adjusted_y) not in coords_dict.keys():
+			coords_dict[(adjusted_x,adjusted_y)] = list()
+		coords_dict[(adjusted_x,adjusted_y)].append(z)
+		freqd[(adjusted_x,adjusted_y)] = freqd.get((adjusted_x,adjusted_y),0)+1	
+		
+	obj_file.close()
+
+	for key, value in coords_dict.iteritems():
+		coords_dict[key] = np.ptp(np.array(value))
+
+
+	outfile = open('filtered_range.obj', 'w')
+	for point in point_info:
+		x = point[0]
+		y = point[1]
+		adjusted_x = int(x//0.3)
+		adjusted_y = int(y//0.3)
+		if coords_dict[(adjusted_x, adjusted_y)]<6:
+			continue
+		line = "v " + str(x) + " " + str(y) + " "+ str(point[2])
+		outfile.write(line)
+		outfile.write("\n")
+	outfile.close()
+
 if __name__=="__main__":
 	fuse_file = 'final_project_data/final_project_point_cloud.fuse'
 	# intensityHistogram(fuse_file)
 	# filterIntensity(fuse_file)
-	filterDistance(4364041.85813,850522.039477)
+	# rangeHistogram()
 
-
+	filterRange()
 
 
 
 
 
 	
-
-	
-# 	print "Lat length: " + len(lat_set)
-# 	print "Long length: " + len(long_set)
-# 	print "Lat min: " + min(lat_set)
-# 	print "Long min: " + min(long_set)
-# 	print "Lat max: " + max(lat_set)
-# 	print "Long max: " + max(long_set)
-# >>>>>>> 9d25bf0509ead8e70a35d1413d620d6a00a9097d
